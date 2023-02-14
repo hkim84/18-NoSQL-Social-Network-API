1d7eff9cdbd36290e55feb49e2c1b6567f200b69
Hoon Kim <eloael@outlook.com>
2023-02-13 23:29:16 -0800

    changed a few things

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -121,4 +121,5 @@ const thoughtController = {
   },
 };
 
+
 module.exports = thoughtController;
--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use(express.static('public'));
 // if MONGODB_URI exists, connect to that DB
 // otherwise short-circuit to local MongoDB server's DB
 // MongoDB finds and connects to DB if exists or creates if it doesn't
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
+    // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  
+    // useCreateIndex: true,
 });
 
 // log mongo queries being executed
